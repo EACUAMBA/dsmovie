126f3eaac3f7da4c1f8b5db75ddc1d4328955723
Edilson Alexandre Cuamba <edilson.cuamba@exi.co.mz>
2022-03-10 16:12:26 +0200

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,27 @@
 import Pagination from "../../components/Pagination";
+import MovieStar from "../../components/MovieStar";
+import MovieScore from "../../components/MovieScore";
+import MovieCard from "../../components/MovieCard";
 
 function Listing() {
     return(
-        <Pagination/>
+        <>
+            <Pagination/>
+
+            <div className={'container'}>
+                <div className={'row'}>
+                    <div className={'col-sm-6 col-lg-4 col-xl-4 mb-3'}>
+                        <MovieCard/>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+            <MovieCard/>
+
+        </>
     )
 }
 export default Listing;
