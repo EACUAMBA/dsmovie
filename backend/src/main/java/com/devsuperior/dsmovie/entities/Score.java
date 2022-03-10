142040c2ee3f980000c5ce4998ca30fcb4edbb7d
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-03-10 21:50:59 +0200

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,54 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+    @EmbeddedId
+    private ScorePK scorePK = new ScorePK();
+    private Double value;
+
+    public Score() {
+    }
+
+    public Score(ScorePK scorePK, Double value) {
+        this.scorePK = scorePK;
+        this.value = value;
+    }
+
+    public ScorePK getScorePK() {
+        return scorePK;
+    }
+
+    public void setScorePK(ScorePK scorePK) {
+        this.scorePK = scorePK;
+    }
+
+    public User getUser(){
+        return this.scorePK.getUser();
+    }
+
+    public void setMovie(Movie movie){
+        this.scorePK.setMovie(movie);
+    }
+
+    public Movie getMovie(){
+        return this.scorePK.getMovie();
+    }
+
+    public void setUser(User user){
+        this.scorePK.setUser(user);
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+}
