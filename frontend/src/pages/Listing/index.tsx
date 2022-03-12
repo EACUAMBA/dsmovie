8ba2fddaa30225cd9533be915a3c7507fb33733c
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-03-12 22:43:19 +0200

    Adicionando _redirects, removendo imports que não uso.

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,13 +1,10 @@
 import Pagination from "../../components/Pagination";
-import MovieStar from "../../components/MovieStar";
-import MovieScore from "../../components/MovieScore";
 import MovieCard from "../../components/MovieCard";
 
 function Listing() {
     return(
         <>
             <Pagination/>
-
             <div className={'container'}>
                 <div className={'row'}>
                     <div className={'col-sm-6 col-lg-4 col-xl-4 mb-3'}>
